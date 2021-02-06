76514f61cdd0500f3a8f1672629a98e80bbb80f0
Merlin <merlinzhao@gmail.com>
2021-02-06 00:28:26 -0500

    Added more components

---
+++ src/components/Demo1.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+
+function Demo1() {
+    return (
+        <div>
+            <h1>MERLIN!!</h1>
+            <p>This is a test</p>
+            <ul>
+                <li>Thing1</li>
+                <li>Thing2</li>
+                <li>Thing3</li>
+            </ul>
+            <main>
+                More content
+            </main>
+
+        </div>
+    )
+}
+
+export default Demo1
